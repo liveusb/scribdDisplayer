586a8b6f8e3cd3a717cec171f04c06129c54f832
BackinR2D2 <cristimlg334@gmail.com>
2020-11-20 20:45:54 +0200

    v1

---
+++ public/unlock.js
@@ -0,0 +1,27 @@
+const btn = document.querySelector('button');
+const url = document.querySelector('input');
+const title = document.querySelectorAll('input')[1];
+const doc = document.querySelector('.doc');
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    fetch('/unlock', {
+        method: 'POST',
+        body: JSON.stringify({
+            url: url.value,
+            title: title.value,
+        }),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+        .then((resp) => resp.json())
+        .then((result) => {
+            url.value = "";
+            title.value = "";
+            const { template } = result;
+            doc.innerHTML = template;
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile('index');
+})
+
+app.post('/unlock', (req, res) => {
+    const { url, title } = req.body;
+    const scribdUrl = new URL(url)
+    const numID = scribdUrl.pathname.split('/')[2];
+    const template = `<iframe class="scribd_iframe_embed" title="${title}" src="https://www.scribd.com/embeds/${numID}/content?start_page=1&view_mode=scroll&access_key=key-1pflhk5nptq303jlxchs" data-auto-height="true" data-aspect-ratio="0.7080062794348508" scrolling="no" width="100%" height="600" frameborder="0"></iframe>`
+    res.json({
+        template
+    })
+})
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+})
